7a6276b59520efb07fd09f9ea70dbd58bb92c690
Chris Altman <chris@altonymous.com>
2013-08-09 03:39:37 -0400

    Missed some renames

--- lib/activerecord-import/adapters/nulldb_adapter.rb
+++ lib/activerecord-import/adapters/nulldb_adapter.rb
@@ -1,5 +1,5 @@
 require File.dirname(__FILE__) + "/nulldb_adapter"
 
-module ActiveRecord::Import::Mysql2Adapter
-  include ActiveRecord::Import::NullDbAdapter
+module ActiveRecord::Import::NullDBAdapter
+  include ActiveRecord::Import::NullDBAdapter
 end
